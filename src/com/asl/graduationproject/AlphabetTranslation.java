490d3bffd0f0cb96db07f3b9f6f4fab5862600ea
MostafaAbass <engmostafa92@gmail.com>
2016-01-01 10:30:38 +0200

    Helpful tools for translation

--- src/com/asl/graduationproject/AlphabetTranslation.java
+++ src/com/asl/graduationproject/AlphabetTranslation.java
@@ -28,6 +28,42 @@ public class AlphabetTranslation extends Listener {
 		
 		if(firstHand.isValid())
 		{
+			FingerList fingers = firstHand.fingers();
+			// fingers ordered from thumb to pinky in each hand 
+			Finger thumbFinger = fingers.get(0) ; 
+			Finger indexFinger = fingers.get(1) ; 
+			Finger middleFinger = fingers.get(2) ;
+			Finger ringFinger = fingers.get(3) ;
+			Finger pinkyFinger = fingers.get(4) ;
+			// bones are ordered from base to tip for each finger
+			// thumb bones from bottom to top 
+			Bone thumbFingerMetacarpalsBone = thumbFinger.bone(Bone.Type.TYPE_METACARPAL); 
+			Bone thumbFingerProximalBone = thumbFinger.bone(Bone.Type.TYPE_PROXIMAL); 
+			Bone thumbFingerIntermediateBone = thumbFinger.bone(Bone.Type.TYPE_INTERMEDIATE); 
+			Bone thumbFingerDistalBone = thumbFinger.bone(Bone.Type.TYPE_DISTAL); 
+			// index bones from bottom to top 
+			Bone indexFingerMetacarpalsBone = indexFinger.bone(Bone.Type.TYPE_METACARPAL); 
+			Bone indexFingerProximalBone = indexFinger.bone(Bone.Type.TYPE_PROXIMAL); 
+			Bone indexFingerIntermediateBone = indexFinger.bone(Bone.Type.TYPE_INTERMEDIATE); 
+			Bone indexFingerDistalBone = indexFinger.bone(Bone.Type.TYPE_DISTAL); 
+			// middle bones from bottom to top 
+			Bone middleFingerMetacarpalsBone = middleFinger.bone(Bone.Type.TYPE_METACARPAL); 
+			Bone middleFingerProximalBone = middleFinger.bone(Bone.Type.TYPE_PROXIMAL); 
+			Bone middleFingerIntermediateBone = middleFinger.bone(Bone.Type.TYPE_INTERMEDIATE); 
+			Bone middleFingerDistalBone = middleFinger.bone(Bone.Type.TYPE_DISTAL); 
+			// ring bones from bottom to top 
+			Bone ringFingerMetacarpalsBone = ringFinger.bone(Bone.Type.TYPE_METACARPAL); 
+			Bone ringFingerProximalBone = ringFinger.bone(Bone.Type.TYPE_PROXIMAL); 
+			Bone ringFingerIntermediateBone = ringFinger.bone(Bone.Type.TYPE_INTERMEDIATE); 
+			Bone ringFingerDistalBone = ringFinger.bone(Bone.Type.TYPE_DISTAL); 
+			// pinky bones from bottom to top 
+			Bone pinkyFingerMetacarpalsBone = pinkyFinger.bone(Bone.Type.TYPE_METACARPAL); 
+			Bone pinkyFingerProximalBone = pinkyFinger.bone(Bone.Type.TYPE_PROXIMAL); 
+			Bone pinkyFingerIntermediateBone = pinkyFinger.bone(Bone.Type.TYPE_INTERMEDIATE); 
+			Bone pinkyFingerDistalBone = pinkyFinger.bone(Bone.Type.TYPE_DISTAL); 
+			//thumbFinger.width()
+			//float angle = thumbFinger.direction().angleTo(indexFinger.direction());
+			//
 			
 		}
 	}
